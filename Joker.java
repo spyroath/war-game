64d295d3773fdff55dda10a272b762cd2d4bfb97
Praia21 <166624830+Praia21@users.noreply.github.com>
2024-11-28 12:26:41 -0500

    Add files via upload

---
+++ Joker.java
@@ -0,0 +1,19 @@
+public class Joker extends Card{
+    
+
+    public Joker(){
+        super(0, 5);
+    }
+
+    public String getSuitAString(){
+        return "Joker";
+    }
+
+    public String getValueAsString(){
+        return "Joker";
+    }
+
+    public String toString() {
+        return "Joker";
+    }
+}
